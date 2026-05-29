4e0519e2c8d845a069f959118375d5b8bc8e57bd
excellar-BX <excellenceay33@gmail.com>
2026-05-29 09:12:59 +0100

    replaced with deployed backend url

--- lasu-news-frontend/src/api/axios.js
+++ lasu-news-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://lasu-news-backend.vercel.app/api",
   withCredentials: true,
 });
 
@@ -56,7 +56,7 @@ api.interceptors.response.use(
 
       try {
         const { data } = await axios.post(
-          `${import.meta.env.VITE_API_URL || "http://127.0.0.1:4000/api"}/auth/refresh`,
+          `${import.meta.env.VITE_API_URL || "https://lasu-news-backend.vercel.app/api"}/auth/refresh`,
           {},
           { withCredentials: true }
         );
